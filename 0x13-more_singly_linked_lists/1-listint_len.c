a7fc7a474b21f6fcfee09b9ee5a2d6735558ac6c
davidolabode <daveolawale03@gmail.com>
2023-08-01 12:31:08 -0700

    elements in a linked listint_t list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+/* linked list */
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+/* while loops */
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
